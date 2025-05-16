e92dbc718dbaee8f200a92065099fa8c29f5fc7e
Achraf <hraf23@gmail.com>
2025-05-16 15:54:09 +0100

    Hello, World

---
+++ chapter_01/exercise_1-1.c
@@ -0,0 +1,16 @@
+/**
+ *
+ * This program prints "Hello, world" message to standard output.
+ *
+ */
+
+#include <stdio.h> // Include standard input/output library for printf.
+
+int main() // The main function - the entry point of the program. 
+{
+    // Call the function printf to print the argument "Hello, world".
+    printf("Hello, world\n");
+
+    // Return 0 to indicate successful program termination.
+    return 0;
+}
