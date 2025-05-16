c7db1875a0b3f7c6c19901b7ca7b3086eda87e9d
Achraf <hraf23@gmail.com>
2025-05-16 16:36:41 +0100

    Unknown escape sequence.

---
+++ chapter_01/exercise_1-2.c
@@ -0,0 +1,18 @@
+/**
+ *
+ * The program prints a character string that contains \c,
+ * as an argument of the printf function.
+ *
+ */
+
+#include <stdio.h> // Standard I/O library for printf.
+
+int main() // The main function - the entry point of the program.
+{
+    // Call the printf function to print the character string argument.
+    printf("Hello\c, world\n");
+
+    // Return 0 to indicate successful program termination.
+    return 0;
+
+}
