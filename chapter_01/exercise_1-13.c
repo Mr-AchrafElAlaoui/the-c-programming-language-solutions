116bed392e3d7b48b930f30165a58f7d3df084fd
Achraf <hraf23@gmail.com>
2025-06-18 23:30:56 +0100

    Update, bucket overflowed words.

--- chapter_01/exercise_1-13.c
+++ chapter_01/exercise_1-13.c
@@ -9,7 +9,7 @@
 
 #define IN 1                    // Inside a word.
 #define OUT 0                   // Outside a word.
-#define MAX_LENGTH_WORD 20      // Maximum word length.
+#define MAX_LENGTH_WORD 21      // Maximum word length.
 
 int main()
 {
@@ -32,9 +32,19 @@ int main()
             if (state == IN)
             {
                 state = OUT;
-                // Substract 1 because word lengths start at 1,
-                // Array indices start at 0.
-                ++lengths_words[count_char - 1];  
+
+                if(count_char <= MAX_LENGTH_WORD)
+                {
+                    // Substract 1 because word lengths start at 1,
+                    // Array indices start at 0.
+                    ++lengths_words[count_char - 1];
+                }
+                else if (count_char > MAX_LENGTH_WORD)
+                {
+                    // Bucket overflowe words.
+                    ++lengths_words[MAX_LENGTH_WORD - 1];
+                }
+
                 count_char = 0;
             }
         
@@ -48,7 +58,14 @@ int main()
 
     for (index = 0; index < MAX_LENGTH_WORD; ++index)
     {
-        printf("%d : ", index + 1);
+        if (index != (MAX_LENGTH_WORD - 1))
+        {
+            printf("%d : ", index + 1);
+        }
+        else
+        {
+            printf("+%d : ", index + 1);
+        }
 
         for(bar = 0; bar < lengths_words[index]; ++bar)
         {
